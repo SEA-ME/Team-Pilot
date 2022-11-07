afca17102d824e1dcc7502cdae54eb83362691cc
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2022-11-07 12:49:10 +0100

    refactor: change dependencies

--- Project-2/CommonAPI-vSomeIP/project-can/src/CANStubImpl.hpp
+++ Project-2/CommonAPI-vSomeIP/project-can/src/CANStubImpl.hpp
@@ -13,18 +13,5 @@ public:
     virtual void GetRPM(const std::shared_ptr<CommonAPI::ClientId> _client, v1::commonapi::CAN::RPM _rpm, GetRPMReply_t _reply);
     virtual void GetSPD(const std::shared_ptr<CommonAPI::ClientId> _client, v1::commonapi::CAN::SPD _spd, GetSPDReply_t _reply);
     virtual void GetBAT(const std::shared_ptr<CommonAPI::ClientId> _client, v1::commonapi::CAN::BAT _bat, GetBATReply_t _reply);
-public:
-    std::uint8_t  qt_HUM();
-    std::int8_t   qt_TMP();
-    std::uint16_t qt_RPM();
-    std::uint8_t  qt_SPD();
-    std::uint8_t  qt_BAT();
-private:
-    v1::commonapi::CAN::HUM canHum;
-    v1::commonapi::CAN::TMP canTmp;
-    v1::commonapi::CAN::RPM canRpm;
-    v1::commonapi::CAN::SPD canSpd;
-    v1::commonapi::CAN::BAT canBat;
 };
 #endif /* CANSTUBIMPL_H_ */
-
