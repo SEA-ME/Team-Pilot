7d0f3f728a43987cde1aaed7da33abed07b2ae62
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2022-11-08 15:04:50 +0100

    refactor: add rpm code in CAN project

--- Project-2/RPM/uno_rpm.ino
+++ Project-2/RPM/uno_rpm.ino
@@ -7,7 +7,7 @@ unsigned long timeold;
 unsigned int pulsesperturn = 4;
 float radius = 0.023;
 float gear_ratio = 3.55;
-float v;
+uint8_t spd;
 
 void counter() {
   pulses++;
@@ -20,7 +20,7 @@ void setup() {
     
     pulses = 0;
     crank_rpm = 0;
-    v= 0;
+    spd = 0;
     timeold = 0;
 }
 
@@ -28,13 +28,13 @@ void loop() {
     if (millis() - timeold >= 1000){
         detachInterrupt(0);
         crank_rpm = (60 * 1000 / pulsesperturn ) / (millis() - timeold) * pulses;
-        v = (3.6 * TWO_PI * crank_rpm * radius) / (60 * gear_ratio);
+        spd = (3.6 * TWO_PI * crank_rpm * radius) / (60 * gear_ratio);
         timeold = millis();
         pulses = 0;
         Serial.print("RPM = ");
         Serial.print(crank_rpm, DEC);
         Serial.print(" Speed = ");
-        Serial.print(v, DEC);
+        Serial.print(spd, DEC);
         Serial.println(" km/h");
         attachInterrupt(0, counter, FALLING);
     }
