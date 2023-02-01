6a3f43a85879247808e5ccf91e04e9339ced8a2c
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2023-02-01 17:58:11 +0100

    fix: fidl syntax changed

---
+++ Project-3/pigpio/ambient.cpp
@@ -0,0 +1,6 @@
+#include "ambient.h"
+
+Ambient::Ambient()
+{
+
+}
---
+++ Project-3/pigpio/ambient.h
@@ -0,0 +1,12 @@
+#ifndef AMBIENT_H
+#define AMBIENT_H
+
+#include <QObject>
+
+class Ambient
+{
+public:
+    Ambient();
+};
+
+#endif // AMBIENT_H
